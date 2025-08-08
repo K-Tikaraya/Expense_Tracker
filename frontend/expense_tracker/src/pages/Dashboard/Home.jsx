edb5ae4a6867f4baf4a79ffccaf6a828a9173e85
K-Tikaraya <kirantikarya2004@gmail.com>
2025-08-08 20:38:44 +0530

    Recent Transactions Section completed

--- frontend/expense_tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense_tracker/src/pages/Dashboard/Home.jsx
@@ -8,6 +8,7 @@ import InfoCard from '../../components/Cards/infoCard';
 import { addThousandSeparator } from '../../utils/helper';
 import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu';
 import {IoMdCard} from "react-icons/io";
+import RecentTransaction from '../../components/Dashboard/RecentTransaction';
 
 const Home = () => {
   useUserAuth();
@@ -18,12 +19,12 @@ const Home = () => {
   const fetchDashboardData = async () => {
     if(loading) return;
     setLoading(true);
-  
 
   try{
     const response = await axiosInstance.get(
       `${API_PATHS.DASHBOARD.GET_DATA }`
     );
+    
     if(response.data){
       setDashboardData(response.data);
     }
@@ -35,19 +36,43 @@ const Home = () => {
 
 };
 
- 
+useEffect(() => {
+  fetchDashboardData();
+}, []);
 
 
   return (
     <DashboardLayout activeMenu = "Dashboard">
       <div className='my-5 mx-auto'> 
         <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+
           <InfoCard
           icon = {<IoMdCard />}
           label="Total Balance"
           value={addThousandSeparator(dashboardData?.totalBalance || 0)}
           color = "bg-primary"
           />
+
+
+          <InfoCard
+          icon = {<LuWalletMinimal />}
+          label="Total Income"
+          value={addThousandSeparator(dashboardData?.totalIncome || 0)}
+          color = "bg-orange-500"
+          />
+
+          <InfoCard
+          icon = {<LuHandCoins />}
+          label="Total Expense"
+          value={addThousandSeparator(dashboardData?.totalExpense || 0)}
+          color = "bg-red-500"
+          />
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
+        <RecentTransaction 
+        transactions = {dashboardData?.recentTransactions}
+        onSeeMore={() => navigate("/expense")}
+        />
         </div>
       </div>
     </DashboardLayout>
